b192da91769833244cb448212d2754c03e892fe0
Alina Dubchak <90031019+AlinaDubchak@users.noreply.github.com>
2023-09-20 18:04:07 +0300

    added try-catch exception

--- Lab1/lab1.java
+++ Lab1/lab1.java
@@ -5,38 +5,42 @@ import java.util.Scanner;
 public class lab1 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        short num = scan.nextShort();
-        double S = 0;
-        System.out.print("Result: \n" + "C2: " + num % 2 + "\n" + "C3: " + num % 3 + "\n" + "C5: " + num % 5 + "\n" + "C7: " + num % 7 + "\n");
+        try {
+            short num = scan.nextShort();
+            double S = 0;
+            System.out.print("Result: \n" + "C2: " + num % 2 + "\n" + "C3: " + num % 3 + "\n" + "C5: " + num % 5 + "\n" + "C7: " + num % 7 + "\n");
 
-        System.out.print("a:");
-        short a = scan.nextShort();
-        System.out.print("n:");
-        short n = scan.nextShort();
-        while (n <= a) {
-            System.out.println("n must be greater than a");
-            n = scan.nextShort();
-        }
-        System.out.print("b:");
-        short b = scan.nextShort();
-        System.out.print("m:");
-        short m = scan.nextShort();
-        while (m <= b) {
-            System.out.println("m must be greater than b");
-            m = scan.nextShort();
-        }
-        scan.close();
-        if (a <= -num % 3 && -num % 3 <= n || b <= 0 && 0 <= m) {
-            System.out.println("ERR. Divide by zero");
-        } else {
-            for (char i = (char) a; i <= n; i++) {
-                for (char j = (char) b; j <= m; j++) {
-
-                    S += ((double) i / j) / (i + num % 3);
-
-                }
+            System.out.print("a:");
+            short a = scan.nextShort();
+            System.out.print("n:");
+            short n = scan.nextShort();
+            while (n <= a) {
+                System.out.println("n must be greater than a");
+                n = scan.nextShort();
             }
-            System.out.println(S);
+            System.out.print("b:");
+            short b = scan.nextShort();
+            System.out.print("m:");
+            short m = scan.nextShort();
+            while (m <= b) {
+                System.out.println("m must be greater than b");
+                m = scan.nextShort();
+            }
+            scan.close();
+            if (a <= -num % 3 && -num % 3 <= n || b <= 0 && 0 <= m) {
+                System.out.println("ERR. Divide by zero");
+            } else {
+                for (char i = (char) a; i <= n; i++) {
+                    for (char j = (char) b; j <= m; j++) {
+
+                        S += ((double) i / j) / (i + num % 3);
+
+                    }
+                }
+                System.out.println(S);
+            }
+        } catch (Exception ex) {
+            System.out.println("Err, invalid type");
         }
     }
 }
