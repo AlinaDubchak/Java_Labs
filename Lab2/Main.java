e42383fb9ecb4332935a051f17cbbb6542785344
alinadubchak <alina.dubchak.ev@gmail.com>
2023-10-06 23:36:33 +0300

    added files for Lab2

---
+++ Lab2/Main.java
@@ -0,0 +1,38 @@
+package Java_Labs.Lab2;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            long[][] matrix = new long[][]{{1,4,6,7},{3,7,2,7}};
+
+            System.out.println("Initial matrix");
+
+            for (int i = 0; i < matrix.length; i++) {
+                for (int j = 0; j < matrix[i].length; j++) {
+                    System.out.print(matrix[i][j] + " ");
+                }
+                System.out.println();
+            }
+
+            TransposedMatrix matrix1 = new TransposedMatrix();
+            long[][] transposedMatrix = matrix1.transpose(matrix);
+
+            System.out.println("Transposed matrix");
+
+            for (int i = 0; i < transposedMatrix.length; i++) {
+                for (int j = 0; j < transposedMatrix[i].length; j++) {
+                    System.out.print(transposedMatrix[i][j] + " ");
+                }
+                System.out.println();
+            }
+
+             TransposedMatrix.average(transposedMatrix);
+
+        } catch (Exception ex) {
+            System.err.println("ERR, please change input. The problem is: \n"
+                    + ex.getMessage());
+        }
+    }
+
+
+}
---
+++ Lab2/TransposedMatrix.java
@@ -0,0 +1,50 @@
+package Java_Labs.Lab2;
+
+//1306
+// C5 = B^T
+// C7 = long
+// Знайти середнє значення елементів кожного рядка матриці
+//
+
+public class TransposedMatrix {
+    public long[][] transpose(long[][] matrix) {
+
+        int numRows = matrix.length;
+        if (numRows == 0) {
+            throw new IllegalArgumentException("Input matrix is empty");
+        }
+        int numCols = matrix[0].length;
+
+        long[][] transposedMatrix = new long[numCols][numRows];
+
+        for (int i = 0; i < numRows; i++) {
+            if (matrix[i] == null || matrix[i].length == 0) {
+                throw new IllegalArgumentException("Row " + i + " is null or empty");
+            }
+            for (int j = 0; j < numCols; j++) {
+                transposedMatrix[j][i] = matrix[i][j];
+            }
+        }
+
+        return transposedMatrix;
+    }
+
+    public static void average(long[][] matrix) {
+
+        for (int i = 0; i < matrix.length; i++) {
+
+            long rowSum = 0;
+            int rowCount = matrix[i].length;
+
+
+            for (int j = 0; j < rowCount; j++) {
+                rowSum += matrix[i][j];
+            }
+
+            double rowAverage = (double) rowSum / rowCount;
+            String result = String.format("%.3f", rowAverage);
+            System.out.println("The average value of the elements in row " + i + ": " + result);
+        }
+    }
+
+}
